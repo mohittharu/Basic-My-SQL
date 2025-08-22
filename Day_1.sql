46dd6748b7bfe5c050c5d366b81d55480d4b8249
mohittharu <mohitchy296@gmail,com>
2025-08-22 22:29:36 +0545

    Day_1

---
+++ Day_1.sql
@@ -0,0 +1,19 @@
+-- 	CREATING DATABASE
+create database aditya_batch_db1;
+
+use aditya_batch_db1; 
+
+
+
+-- CREATING Datacase including TABLE WITH TABLE_NAME
+CREATE TABLE student(
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    grade VARCHAR(10),
+    email VARCHAR(100)
+);
+
+-- CREATES BLOCK FOR THE DETIAL OF THE STUDENT
+describe student;
